29b5c357060c38f67e8de12fa1e9dea209f1ecae
wei chen <weichen9083@gmail.com>
2021-06-16 17:24:51 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  
+  get '/' do
+    erb :index 
+  end 
+  
+  post '/checkout' do
+   @sessions = session
+  @sessions[:item] = params["item"]
+  
+  end 
+configure do
+  enable :sessions
+  set :session_secret, "secret"
+end
 end
