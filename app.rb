7455e8ec4df0c82ac82deffb0907b1befbc64e41
wei chen <weichen9083@gmail.com>
2021-06-16 17:19:48 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  
+  get '/' do
+    erb :index 
+  end 
+  
+  post '/checkout' do
+   session[item]=params[name]
+   @session = session
+  end 
+configure do
+  enable :sessions
+  set :session_secret, "secret"
+end
 end
